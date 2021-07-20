970fe780d03f8923942b0e533a2698af92f7ade9
Lassi Kortela <lassi@lassi.io>
2021-07-20 23:07:48 +0300

    But don't put the top index.html in a subdirectory

--- www.sh
+++ www.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 mkdir -p www
-for md in $(cd surveys && ls *.md | sort -f); do
+pandoc --from=gfm "surveys/index.md" -o "www/index.html"
+for md in $(cd surveys && ls *.md | sort -f | grep -v index.md); do
     base="${md%.*}"
     mkdir -p "www/$base"
     pandoc --from=gfm "surveys/$md" -o "www/$base/index.html"
