1afb7a8ecf27baf4961b8855c5a36de38fcc70e3
Lassi Kortela <lassi@lassi.io>
2021-07-20 23:05:27 +0300

    Put each survey in its own subdirectory
    This lets us drop "index.html" from the URLs.

--- www.sh
+++ www.sh
@@ -3,6 +3,6 @@ cd "$(dirname "$0")"
 mkdir -p www
 for md in $(cd surveys && ls *.md | sort -f); do
     base="${md%.*}"
-    html="$base.html"
-    pandoc --from=gfm "surveys/$md" -o "www/$html"
+    mkdir -p "www/$base"
+    pandoc --from=gfm "surveys/$md" -o "www/$base/index.html"
 done
